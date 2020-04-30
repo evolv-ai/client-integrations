0462be9884e36cbf5cfcc2a126bdd8243a51767c
Teo Lisitza <teolisitza@teo-lisitza-macbook-pro.attlocal.net>
2020-04-30 07:57:56 -0700

    More tweaks, clear out real GA codes

--- evolv_ga_example_code.js
+++ evolv_ga_example_code.js
@@ -1,41 +1,41 @@
-    function evolvGA(id, metrics, namespace) {
-        var ga = window.ga;
-        if (!ga) {
-            console.error('NO GA!!');
-            return;
-        }
-        var prefix = namespace ? namespace + '.' : '';
-        try {
-            ga('create', id, 'auto', namespace ? { namespace } : null);
-            for (const dimension in metrics) {
-                ga(prefix + 'set', 'dimension'+dimension, metrics[dimension]);
-            }
-            ga(prefix + 'send', 'event', 'evolv', 'evolvRendered', { nonInteraction: true });
-        } catch (e) {
-            console.error(e.toString());
-        }
+function evolvGA(id, metrics, namespace) {
+    var ga = window.ga;
+    if (!ga) {
+        console.error('NO GA!!');
+        return;
     }
-
-    function evolvLog(e) {
-        evolv.runtime.then(function(runtime) {
-            var evolvUID = runtime.audience.cookie['evolv:uid']
-            var evolvCID = e.planId.split(':')[0];
-            var evolvEID = e.planId.split(':')[1];
-            console.log(evolvUID, evolvCID, evolvEID);
-
-            // Google Analytics - replace values
-            evolvGA('UA-164633832-1', {'1': evolvEID, '2': evolvCID, '3': evolvUID});
-
-            evolvPreload.addLogEntry('ga', e);
-        });
+    var prefix = namespace ? namespace + '.' : '';
+    try {
+        ga('create', id, 'auto', namespace ? { namespace } : null);
+        for (const dimension in metrics) {
+            ga(prefix + 'set', 'dimension'+dimension, metrics[dimension]);
+        }
+        ga(prefix + 'send', 'event', 'evolv', 'evolvRendered', { nonInteraction: true });
+    } catch (e) {
+        console.error(e.toString());
     }
+}
+
+function evolvLog(e) {
+    evolv.runtime.then(function(runtime) {
+        var evolvUID = runtime.audience.cookie['evolv:uid']
+        var evolvCID = e.planId.split(':')[0];
+        var evolvEID = e.planId.split(':')[1];
+        console.log(evolvUID, evolvCID, evolvEID);
+
+        // Google Analytics - replace values
+        evolvGA('UA-XXXXXXXXX-Y', {'A': evolvEID, 'B': evolvCID, 'C': evolvUID});
+
+        evolvPreload.addLogEntry('ga', e);
+    });
+}
 
 window.evolvPreload = {
     listeners: {
         initialize: function(event) {
             addLogEntry('initialize', event);
-                        
         },
+
         initialized: function(event) {
             addLogEntry('initialized', event);
         },
@@ -80,21 +80,5 @@ let num = 1;
 
 function addLogEntry(type, message) {
     message = (typeof message === 'string') ? message : JSON.stringify(message);
-
-    const tbody = document.querySelector('#logOutput');
-    const row = document.createElement('tr');
-    row.innerHTML = `
-        <th scope="row">${num}</th>
-        <td>
-            <time>${new Date().toISOString()}</time>
-        </td>
-        <td>${type}</td>
-        <td><pre>${message}</pre></td>
-    `;
-
-    tbody.appendChild(row);
-
-    num += 1;
-
 	console.log(type, message);
 }
