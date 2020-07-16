86326d2daabcbd1729dfbe265b3919eb4e31cf4c
Dylan MacDonald <54418352+dylanmac-evolv@users.noreply.github.com>
2020-07-16 16:50:30 -0700

    Create experience-accelerator-sample-conversion-goal-code.js

---
+++ events/conversion/experience-accelerator-sample-conversion-goal-code.js
@@ -0,0 +1,17 @@
+// Evolv 1.0 Experience Accelerator Conversion Integration Code
+
+/****
+ * Experience Accelerator click event. 
+ * Reference the appropriate selector.
+ ****/
+var selector = document.querySelector("[selector]");
+selector.addEventListener("click", function() {
+    evolv.client.emit('[my-event]');
+})
+
+/****
+ * Experience Accelerator trigger of conversion on page load event, e.g. order confirmation.
+ ****/
+document.addEventListener("DOMContentLoaded", function() {
+    evolv.client.emit('[my-event]');
+});
