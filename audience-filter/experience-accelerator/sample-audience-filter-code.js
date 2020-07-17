9a30818b3d80b27508bb856a4af8e6caebf992f2
Dylan MacDonald <54418352+dylanmac-evolv@users.noreply.github.com>
2020-07-17 15:02:18 -0700

    New files

---
+++ audience-filter/experience-accelerator/sample-audience-filter-code.js
@@ -0,0 +1,35 @@
+/****
+ * Evolv 1.0 Audience Filter code for Experience Accelerator and Execution Plan.
+ * Note: the sample code below should be placed AFTER 
+ * the execution plan/experience accelerator snippet in the DOM.
+ ****/
+
+/****
+ * EXECUTION PLAN
+ * Sample audience filter. 
+ * Note: (await evolv.runtime) is not compatible with IE 11
+ ****/
+evolv.runtime.then(function(rt) {
+    rt.updateAudience({
+        // Namespace object will be included in event defintion in the Evolv Manager.
+        // Please inform Evolv if you plan to use a namespace object.
+        [namespacedObject]: {
+            [key]: "[value]" //values are strings only
+        }
+    });
+    // re-run evaluatePlans to apply filters to experiment
+    rt.evaluatePlans();
+});
+
+/****
+ * EXPERIENCE ACCELERATOR
+ * Sample audience filter.
+ * Note: (await evolv.runtime) is not compatible with IE 11
+ ****/
+evolv.context.update({
+    // Namespace object will be included in event defintion in the Evolv Manager.
+    // Please inform Evolv if you plan to use a namespace object.
+    [namespaceObj]: {
+        [key]: "[value]"
+    }
+});
