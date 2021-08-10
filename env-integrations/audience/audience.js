02001d15a5070f89b121b7fb0360ec34f6feb0ee
rcowin <richard.cowin@gmail.com>
2021-08-10 09:14:38 -0500

    add support for fetch with expressions

--- env-integrations/audience/audience.js
+++ env-integrations/audience/audience.js
@@ -4,23 +4,49 @@ function refreshAudience(){
   window.evolv.context.update(audience);
 }
 
+var MacroSet = {
+  map: function(array){
+    return function(tokens){
+      return array.map(function(n){return adapters.getExpressionValue(tokens, n)})
+    }
+  }
+};
+function bindMacro(macro, data){
+  var fnc = MacroSet[macro];
+  if (!fnc){
+    return function(){ console.info('Warning: ', macro, ' not a valid macro');}
+  }
+  return fnc(data)  
+}
+
+function tokenizeExp(exp){
+  return Array.isArray(exp) ? exp : exp.split('.')
+}
+
 var adapters = {
-  expValue(exp){
-    var tokens = exp.split('.');
-    var result = window;
+  getExpressionValue(exp, context){
+    var tokens = tokenizeExp(exp);
+    var result = context || window;
     
     if (tokens[0] === 'window') tokens = tokens.slice(1);
 
     while(tokens.length > 0 && result){
       var token = tokens[0];
       tokens = tokens.slice(1)
-
-      if (token.indexOf('(')> 0){
+      var openPos = token.indexOf('(');
+      if (openPos > 0){
         try{
-          token = token.slice(0,-2);
+          var closingPos = token.indexOf(')')
+          var param = token.slice(openPos+1, closingPos)
+          token = token.slice(0,openPos);
           var fnc = result[token]
-          if (typeof fnc === 'function')
-            result = fnc.apply(result, []);
+
+          if (typeof fnc === 'function'){
+            result = fnc.apply(result, [param]);
+          } else if (token[0] === '@'){
+            var macro = bindMacro(token.slice(1), result);
+            result = macro(param)
+          }
           else
             result = undefined;
         }
@@ -35,26 +61,71 @@ var adapters = {
     }
     return result;
   },
-  cookieValue: function(name) {
+  setExpressionValue: function(exp, values, append){
+    var tokens = tokenizeExp(exp)
+    var key = tokens.pop();
+    var obj = adapters.getExpressionValue(tokens);
+    
+    if (!obj) return;
+  
+    if (append){
+      obj[key] += values;
+    } else{
+      obj[key] = values;
+    }
+  },
+  getFetchValue: function(fetchData) {
+    var url = fetchData.url || '';
+    var method = fetchData.method || 'POST'
+    var data  = fetchData.data || {};
+    return fetch(url, {
+        method: method,
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    })
+    .then(function(response){return response.json();})
+    .then(function(response){
+      var expression = fetchData.expression || null;
+      var store = fetchData.bindTo || null;
+      var data = response.data;
+      var value = adapters.getExpressionValue(expression, data);
+      if (store){
+        adapters.setExpressionValue(store, data);
+      }
+      if (!value) return '';
+      
+      return value.toString();
+    })
+  },
+  getCookieValue: function(name) {
     var cookie = document.cookie.split(';')
         .find(function(item) {return item.trim().split('=')[0] === name })
     if (!cookie) return null;
 
     return cookie.split('=')[1];
   },
-  domValue: function(sel) {
+  getDomValue: function(sel) {
     return document.querySelector(sel)  && 'found';
   },
-  jqdomValue: function(sel) {
+  getJqDomValue: function(sel) {
     return window.$ && ($(sel).length > 0)  && 'found';
   }
 }
 
 function getValue(obj){
-  if (obj.type === 'expression') return adapters.expValue(obj.value);
-  if (obj.type === 'cookie') return adapters.cookieValue(obj.value);
-  if (obj.type === 'dom') return adapters.domValue(obj.value);
-  if (obj.type === 'jqdom') return adapters.jqdomValue(obj.value);
+  if (obj.type === 'expression') return adapters.getExpressionValue(obj.value);
+  if (obj.type === 'fetch') return adapters.getFetchValue(obj.value);
+  if (obj.type === 'dom') return adapters.getDomValue(obj.value);
+  if (obj.type === 'jqdom') return adapters.getJqDomValue(obj.value);
+  if (obj.type === 'cookie') return adapters.getCookieValue(obj.value);
+
   return null;
 }
 
@@ -67,7 +138,7 @@ function initConfig(){
   var distribution = window.localStorage.getItem(distributionName);
   if (!distribution) {
     distribution = Math.floor(Math.random()*100);
-    window.localStorage.setItem(distributionName, distribution)
+    window.localStorage.setItem(distributionName, distribution);
   }
 
   var qaAudienceEnabled = window.localStorage.getItem('evolv:qa_audience_enabled')
@@ -81,17 +152,24 @@ function initConfig(){
 
 function addAudience(topKey, key, obj){
   function bindAudienceValue(val, inc){
-    audience[topKey][key] = convertValue(val)
+    audience[topKey][key] = convertValue(val);
     if (inc){
        audience[topKey][key + '_pollingCount'] = '' + inc;
     }
   }
   try{
     var val = getValue(obj);
-
     if (val){
-      bindAudienceValue(val)
-      return;
+      if (val.then) {//assume is promise
+        val.then(function(response){
+          bindAudienceValue(response, 0);
+          refreshAudience();
+        })
+        return;
+      } else {
+        bindAudienceValue(val);
+        return;
+      }
     }
 
     if (!obj.poll){
@@ -109,12 +187,11 @@ function addAudience(topKey, key, obj){
           refreshAudience();
           clearInterval(poll);
         }
-      } catch(e){console.info('audience not processed', obj)}
+      } catch(e){console.info('audience not processed', obj);}
     }, obj.poll.interval || 50)
-    setTimeout(function(){ clearInterval(poll)}, obj.poll.duration || 250)
-
+    setTimeout(function(){ clearInterval(poll)}, obj.poll.duration || 250);
   } catch(e){
-    console.warn('Unable to add audience for', topKey, key, obj, e)
+    console.warn('Unable to add audience for', topKey, key, obj, e);
   }
 }
 
@@ -124,7 +201,6 @@ function setAudience(json){
     topKeys.forEach(function(topKey){
       var namespace = json[topKey];
       if (typeof namespace !== 'object'){
-        console.warn('invalid audience namespace for', topKey)
         return;
       }
       
@@ -143,4 +219,20 @@ function setAudience(json){
 
 audience.config = initConfig();
 
-module.exports = setAudience;
+//module.exports = setAudience;
+
+//test data
+setAudience({
+  "project": {
+    "promotype": {
+      "page": "sales/digital/expressCheckout.html",
+      "type": "fetch",
+      "value": {
+        "method": "POST",
+        "url": "https://www.verizon.com/soe/digital/auth/orderprocessservice/checkoutapi/get-checkout-details",
+        "expression": "cart.lineDetails.lineInfo.@map(promoType)",
+        "bindTo": "window.evolv.promoType"
+      }
+    }
+  }
+})
