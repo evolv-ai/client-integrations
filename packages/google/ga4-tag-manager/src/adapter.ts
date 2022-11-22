6c3d4afeb5138a1a1e9179a7117eb616cf28c170
joshua-barnard-evolv <78709720+joshua-barnard-evolv@users.noreply.github.com>
2022-11-22 11:47:07 +1000

    Modified getHandler and get Analytics

--- packages/google/ga4-tag-manager/src/adapter.ts
+++ packages/google/ga4-tag-manager/src/adapter.ts
@@ -8,11 +8,11 @@ export class TagManagerAdapter extends AnalyticsNotifierAdapter {
 	}
 
 	getAnalytics() {
-		return window.dataLayer.push;
+		return window.google_tag_manager;
 	}
 
 	getHandler() {
-		return this.getAnalytics();
+		return window.dataLayer.push;
 	}
 
 	checkAnalyticsProviders() {
@@ -28,7 +28,7 @@ export class TagManagerAdapter extends AnalyticsNotifierAdapter {
 			}
 		});
 
-		if (ga4Config)	console.log('Evolv: GA4 config not detected. Please ensure you have added a GA4 config tag to your GTM container.');
+		if (!ga4Config)	console.log('Evolv: GA4 config not detected. Please ensure you have added a GA4 config tag to your GTM container.');
 	};
 
 	sendMetrics(type: string, event: any) {
