2cdade2cb2e7827453c4274196b7780b9102c127
joshua-barnard-evolv <78709720+joshua-barnard-evolv@users.noreply.github.com>
2022-12-13 13:35:14 +1000

    Added context checks for events

--- packages/google/ga4-tag-manager/src/adapter.ts
+++ packages/google/ga4-tag-manager/src/adapter.ts
@@ -49,9 +49,14 @@ export class TagManagerAdapter extends AnalyticsNotifierAdapter {
 
 		let evolvEvent = 'evolv';
 
+		window.evolv.context.set('GA4_Sending', true);
+		window.evolv.context.set('GA4_Handler_ready', !!this.getHandler());
+
 		this.emit({
 			event: evolvEvent,
 			evolvEventDetails: { ...dataMap }
 		});
+
+		window.evolv.context.set('GA4_Sent', true);
 	}
 }
