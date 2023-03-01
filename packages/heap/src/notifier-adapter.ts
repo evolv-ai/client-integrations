21939fd4398c2cda8a237cbfcc4ff12e7a9953b7
rcowin <richard.cowin@gmail.com>
2023-03-01 16:29:16 -0600

    added track function as target for heap events

--- packages/heap/src/notifier-adapter.ts
+++ packages/heap/src/notifier-adapter.ts
@@ -10,7 +10,7 @@ export class HeapNotifierAdapter extends AnalyticsNotifierAdapter {
     }
 
     getAnalytics() {
-        return window.heap;
+        return window.heap.track;
     }
 
     getHandler(): AnalyticsHandler {
